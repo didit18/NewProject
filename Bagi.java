8d34ed4c458dd2ac4dabfc74c046a6fb210885f8
didit18 <diditmeidi18@gmail.com>
2014-09-26 09:02:05 +0700

    Project

---
+++ Bagi.java
@@ -0,0 +1,12 @@
+import java.util.*
+calass Bagi{
+	public static void main (String args[]) {
+	Scanner input = new Scanner (System.in)
+	int a,b,hasil;
+	System.out.print("Masukkan Nilai A = ");
+	a = input.nextint();
+	b = input.nextint();
+	hasil = a/b;
+System.out.print("Hasil = " + hasil);
+}
+}
